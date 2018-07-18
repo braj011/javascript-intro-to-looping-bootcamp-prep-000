9808790a1dc63130dfa375ef2b668b2fe753543d
braj011 <bhagyaraj01@gmail.com>
2018-07-18 17:10:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,18 +7,11 @@ for ([initialization]; [condition]; [iteration]) {
 function forLoop (array) {
   for (let i = 0, i < 25, i++) {
     if (i ==1) {
-      console.log("I am 1 ")
+      console.log("I am 1 strange loop.")
+    } 
+    else {
+      console.log(`I am ${i} strange loops.`)
+    } 
     }
-    console.log("I am ${i} ")
-  }
-  
-  (var i = 1; i < 100; i++) {
-  console.log( "Hello World the " + i + " time" );
-}
-
-
-if (hour < 18) {
-    greeting = "Good day";
-} else {
-    greeting = "Good evening";
-}
+    }
+   
